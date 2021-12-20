87be6317e82747e01fd1f82938d1faeb1609ab87
Phil Mercurio <mercurio@thyrd.org>
2021-12-20 11:15:59 -0800

    Renames buttons, adds view source
    Renames the buttons to be clearer and adds a button to view the
    source of the current demo on Github.

--- src/components/TopHeader.svelte
+++ src/components/TopHeader.svelte
@@ -6,14 +6,35 @@
 
 	export let currentRoute
 
-	const txt = {
-		'/': 'Demonstrates a simple chart of static data',
-		'/module_chart': 'Demonstrates explicitly importing ZingChart modules',
-		'/module_drag': 'Demonstrates interacting with a chart to change data',
-		'/dynamic': 'Demonstrates modifying the configuration of an existing chart',
-		'/events': 'Demonstrates responding to chart events',
-		'/methods': 'Demonstrates using a reference to a ZingChart element to invoke methods on it',
-		'/license': 'Demonstrates setting the license key and performance flags on the ZingChart object, as well as multiple plots in one chart'
+	const modules = {
+		'/': {
+			text: 'Demonstrates a simple chart of static data',
+			link: 'https://github.com/zingchart-demos/zingchart-svelte-demo/blob/main/src/components/Simple.svelte'
+		},
+		'/module_chart': {
+			text: 'Demonstrates explicitly importing ZingChart modules',
+			link: 'https://github.com/zingchart-demos/zingchart-svelte-demo/blob/main/src/components/ModuleChart.svelte'
+		},
+		'/module_drag': {
+			text: 'Demonstrates interacting with a chart to change data',
+			link: 'https://github.com/zingchart-demos/zingchart-svelte-demo/blob/main/src/components/ModuleDrag.svelte'
+		},
+		'/dynamic': {
+			text: 'Demonstrates modifying the configuration of an existing chart',
+			link: 'https://github.com/zingchart-demos/zingchart-svelte-demo/blob/main/src/components/Dynamic.svelte'
+		},
+		'/events': {
+			text: 'Demonstrates responding to chart events',
+			link: 'https://github.com/zingchart-demos/zingchart-svelte-demo/blob/main/src/components/Events.svelte'
+		},
+		'/methods': {
+			text: 'Demonstrates using a reference to a ZingChart element to invoke methods on it',
+			link: 'https://github.com/zingchart-demos/zingchart-svelte-demo/blob/main/src/components/Methods.svelte'
+		},
+		'/license': {
+			text: 'Demonstrates setting the license key and performance flags on the ZingChart object, as well as multiple plots in one chart',
+			link: 'https://github.com/zingchart-demos/zingchart-svelte-demo/blob/main/src/components/License.svelte'
+		},
 	}
 
 </script>
@@ -21,15 +42,16 @@
 <div class="header">
 	<h2>zingchart-svelte Demo</h2>
 	<div class="buttonbar">
-		<NavbarItem {currentRoute} path="/" label="Simple" />
-		<NavbarItem {currentRoute} path="/module_chart" label="Module Chart" />
-		<NavbarItem {currentRoute} path="/module_drag" label="Module Drag" />
-		<NavbarItem {currentRoute} path="/dynamic" label="Dynamic" />
+		<NavbarItem {currentRoute} path="/" label="Hello World" />
+		<NavbarItem {currentRoute} path="/module_chart" label="US Map" />
+		<NavbarItem {currentRoute} path="/module_drag" label="Interaction" />
+		<NavbarItem {currentRoute} path="/dynamic" label="Reconfiguring" />
 		<NavbarItem {currentRoute} path="/events" label="Events" />
 		<NavbarItem {currentRoute} path="/methods" label="Methods" />
-		<NavbarItem {currentRoute} path="/license" label="License" />
+		<NavbarItem {currentRoute} path="/license" label="Multiple Plots" />
 	</div>
-	<h4>{txt[currentRoute.name]}</h4>
+	<h4>{modules[currentRoute.name].text}</h4>
+	<div class="viewsrc"><a target="_blank" href={modules[currentRoute.name].link}>View source on Github</a></div>
 </div>
 
 <style>
@@ -46,6 +68,29 @@
 
 .header h2 {
 	color: #4CAF50;
+}
+
+.header h4 {
+	margin-bottom: 0px;
+}
+
+.viewsrc {
+	margin-top: 0px;
+	background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 5px 10px;
+  text-align: center;
+  display: inline-block;
+  font-size: 14px;
+  border-radius: 8px;
+  text-decoration: none;
+	margin-top: 0.5em;
+	margin-bottom: 1em;
+}
+
+.viewsrc a {
+	color: white;
 }
 
 .buttonbar {
